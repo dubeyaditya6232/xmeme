7e7b82579d2006ef14a1ce022c268364e13e9414
Saurav Priyadarshi <psaurav1290@gmail.com>
2022-10-14 23:10:42 +0530

    Add: profile route to view user profile

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,66 @@
-var express = require("express");
-var router = express.Router();
-const jwt = require("jsonwebtoken");
 const Users = require("../model/users");
-const { getUsers, login, register } = require("../controllers/user");
-/* GET users listing. */
-router.get("/", getUsers);
+const bcrypt = require("bcrypt");
+const authenticate = require("../middleware/authenticate");
+const jwt = require("jsonwebtoken");
 
-router.post("/login", login);
+exports.getUsers = (req, res, next) => {
+  res.send("respond with a resource");
+};
 
-router.post("/register", register);
+exports.login = async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    if (!user) {
+      res.statusCode = 404;
+      return res.json({ message: "User not Found", data: null });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (validPassword) {
+      token = jwt.sign({ _id: user._id, email }, "secretKey", {
+        expiresIn: 3600,
+      });
+      res.status(200);
+      return res.json({ message: "success", data: user, token });
+    } else {
+      res.statusCode = 400;
+      return res.json({ message: "Email and password not match", data: null });
+    }
+  } catch (err) {
+    return res.json({ message: err.message, data: null });
+  }
+};
 
-module.exports = router;
+exports.register = async (req, res, next) => {
+  try {
+    let { email, name, password } = req.body;
+    const user = await Users.findOne({ email });
+
+    if (user) {
+      res.statusCode = 409;
+      return res.json({ message: "User already exists", data: null });
+    } else {
+      const newUser = new Users({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      newUser.password = await bcrypt.hash(newUser.password, salt);
+      const data = await newUser.save();
+      res.statusCode = 200;
+      return res.json({ message: "Success! User added!", data });
+    }
+  } catch (err) {
+    return res.json({ message: err.message, data: null });
+  }
+};
+
+exports.getProfile = async (req, res, next) => {
+  try {
+    const data = await Users.findById({ _id: req.params.id });
+    res.statusCode = 200;
+    res.json({ msg: "Success", data });
+  } catch (err) {
+    console.log(err);
+  }
+};
