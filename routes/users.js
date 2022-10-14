01553e7a878f0a5edbf4ec32d64222d430e42d05
Saurav Priyadarshi <psaurav1290@gmail.com>
2022-10-14 22:36:58 +0530

    Add: profile route to view user profile

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,14 @@ var express = require("express");
 var router = express.Router();
 const jwt = require("jsonwebtoken");
 const Users = require("../model/users");
-const { getUsers, login, register } = require("../controllers/user");
+const { getUsers, login, register, getProfile } = require("../controllers/user");
 /* GET users listing. */
 router.get("/", getUsers);
 
 router.post("/login", login);
 
 router.post("/register", register);
+
+router.get("/profile/:id", getProfile)
 
 module.exports = router;
