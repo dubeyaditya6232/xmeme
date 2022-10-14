4e06c3588947c466030b26bf1a12546f6b6a814f
Saurav Priyadarshi <psaurav1290@gmail.com>
2022-10-14 22:26:58 +0530

    Add: meme update count in database

--- model/meme.js
+++ model/meme.js
@@ -1,26 +1,29 @@
 require('dotenv').config();
 const { NotExtended } = require('http-errors');
-const mongoose=require('mongoose');
-const schema=mongoose.Schema;
-const autoIncrement=require('mongoose-auto-increment');
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
 
 mongoose.set('useCreateIndex', true);
 
 var connection = mongoose.createConnection("mongodb://127.0.0.1/xmeme");
 autoIncrement.initialize(connection);
-const memeSchema=new schema({
-    name:{
-        type:String,
-        //required:true
-    },
-    caption:{
-        type:String,
-        //required:true
-    },
-    url:{
+const memeSchema = new schema({
+    name: {
         type: String,
         //required:true
+    },
+    caption: {
+        type: String,
+        //required:true
+    },
+    url: {
+        type: String,
+        //required:true
+    },
+    counter: {
+        type: Number,
     }
 });
-memeSchema.plugin(autoIncrement.plugin,'meme');
+memeSchema.plugin(autoIncrement.plugin, 'meme');
 module.exports = new mongoose.model('meme', memeSchema);
